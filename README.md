424e360cd861d3095791774ee4cf6a6a2154e8c1
you know who <chorvickprocode@gmail.com>
2021-02-22 00:06:30 -0500

    added .gitignore and made skeleton of prompts, prompts work

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# generatereadme
+ 
--- index.js
+++ index.js
@@ -1,2 +1,70 @@
-const inquirer = require('inquirer');
+const inquirer = require('inquirer');   // package required to install on command line npm init -y then : npm i inquirer
 const fs = require('fs');
+
+
+
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project ? ',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project ... ',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Please provide directions to install your project ... ',
+        },
+        {
+            type: 'input',
+            name: 'user',
+            message: 'Please enter your user story for the project... ',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please explain the contribution guidelines for the project... ',
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'Please provide testing instructions for the project... ',
+
+        },
+        {
+            type: 'input',
+            name: 'githubName',
+            message: 'Please enter your Github user name... ',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your e mail address... ',
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'Please provide testing instructions for the project... ',
+
+        },
+
+
+
+
+    ]);
+const generateMd = (type) => ` `
+
+
+
+const initalize = () => {
+    promptUser().then((type) => {
+        const md = generateMd(type);
+        fs.writeFileSync('README.md', md);
+    });
+}
+initalize();
